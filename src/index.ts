bda28358d6b711b79019f600338feda11e6dc6d0
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-02-26 21:27:32 +0530

    code added

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Response, Request } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/check", (req: Request, res: Response) => {
+    res.send("Server is running");
+});
+
+app.listen(port, () => [
+    console.log(`Server is running on port ${port}`)
+]);
