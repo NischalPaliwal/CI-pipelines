fb74593ee572fb7503bae9b9c6a91f9877c9c380
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-02-26 21:33:59 +0530

    code added

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,6 @@ app.get("/check", (req: Request, res: Response) => {
     res.send("Server is running");
 });
 
-app.get("/", (req: Request, res: Response) => {
-    res.json({
-        message: "Hi there from the server!"
-    });
-});
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
